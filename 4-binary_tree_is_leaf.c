c107b2b20e57a1068eba7ada019554cc43a14401
Paul Ogolla <paulotieno2@gmail.com>
2024-04-11 15:59:25 +0300

    leaf node

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - is a node
+ * @node: node
+ * Return: 0/1
+*/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/* leaf node has no children */
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
