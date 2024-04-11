e4e4846c0bf4d2c765cbd9dc96bd9a58fa4b0f94
Paul Ogolla <paulotieno2@gmail.com>
2024-04-11 18:23:53 +0300

    root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - find root
+ * @node: pointer to node
+ * Return: 0 or 1
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
+}
